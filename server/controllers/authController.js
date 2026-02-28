06fb2744c1a926b8abd1d4de40454e054bfbb528
pavanraj8943 <pavan.64j@gmail.com>
2026-02-28 14:33:31 +0530

    auth updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -126,6 +126,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   if (process.env.NODE_ENV === 'production') {
     options.secure = true;
+    options.sameSite = 'none';
   }
 
   res
@@ -189,10 +190,17 @@ exports.googleLogin = async (req, res) => {
 // @route   GET /api/auth/logout
 // @access  Private
 exports.logout = async (req, res) => {
-  res.cookie('token', 'none', {
+  const options = {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true
-  });
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+    options.sameSite = 'none';
+  }
+
+  res.cookie('token', 'none', options);
 
   res.status(200).json({
     success: true,
